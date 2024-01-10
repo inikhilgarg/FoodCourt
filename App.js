8735a3214a5518befccb502c34be46448a23d7ea
Nikhil Garg <gargnikhil104@gmail.com>
2024-01-11 01:40:00 +0530

    initialing package

--- App.js
+++ App.js
@@ -1,3 +1,6 @@
+import React from "react"
+import ReactDOM from "react-dom"
+
 const h1=React.createElement('h1',{},"hello world using react")
 const root=ReactDOM.createRoot(document.getElementById('root'))
 root.render(h1)
