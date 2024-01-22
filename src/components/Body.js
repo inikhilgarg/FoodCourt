b1c7655f6e1deca58a1e2d388fc10b6523fabc1b
Nikhil Garg <gargnikhil104@gmail.com>
2024-01-22 05:54:00 +0530

    fetch data and search functionality added

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,19 +1,35 @@
 import RestuarantCard from "./RestuarantCard";
-import { restaurants } from "../utils/mockData";
 import { useEffect, useState } from "react";
+import Shimmer  from "./Shimmer"
+
 
 const Body=()=>{
-    const [res,setRes]=useState(restaurants)
-    const [filter,setFilter]=useState(restaurants)
+    const [res,setRes]=useState([])
+    const [filter,setFilter]=useState([])
     const [search,setSearch]=useState("")
+
+    const fetchData=async()=>{
+        const data= await fetch("https://www.swiggy.com/mapi/homepage/getCards?lat=28.4089123&lng=77.3177894")
+        const jsonData=await data.json();
+        console.log(jsonData?.data?.success?.cards[3]?.gridWidget?.gridElements?.infoWithStyle?.restaurants)
+        const resData=jsonData?.data?.success?.cards[3]?.gridWidget?.gridElements?.infoWithStyle?.restaurants;
+        setRes(resData)
+        setFilter(resData)
+    }
+    useEffect(()=>{
+        console.log("useEffect")
+        fetchData();
+    },[])
+    console.log("Body rendered")
+    if(res.length===0) return <Shimmer/>
+    
     return(    
         <div className="body-container">
             <div className="search-container">
                 <input type="text" value={search} onChange={(e)=>{setSearch(e.target.value)}} />
                 <button onClick={()=>{
-                    setFilter((prev)=>{
-                        return prev.filter((e)=>e.info.name.toLowerCase().includes(search.toLowerCase()))
-                    })
+                    setFilter(res.filter((e)=>e.info.name.toLowerCase().includes(search.toLowerCase())))
+                    
                 }}>Search</button>
                 <button onClick={()=>{
                     setFilter((prev)=>{
